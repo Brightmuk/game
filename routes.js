2aa4e476b5ec663ee750bce1e2b13193b547abfe
Brightmuk <be.rightmuk@gmail.com>
2020-04-14 01:28:20 +0300

    create chat pages

--- routes.js
+++ routes.js
@@ -1,5 +1,6 @@
 const {getHomePage,layout} = require('./routes/index');
 const {userLogin,userLoginPage,userSignup,userSignupPage}=require('./routes/authentication')
+const {chatsPage,sendMessage,newChat,oneChat}=require('./routes/chat');
 module.exports =function(app,passport){
 
     app.get('/' ,getHomePage);
@@ -7,5 +8,9 @@ module.exports =function(app,passport){
     app.post('/logsin',userLogin);
     app.get('/login',userLoginPage);
     app.post('/signsup',userSignup);
-    app.get('/signup',userSignupPage)
+    app.get('/signup',userSignupPage);
+    app.get('/chat:user_id',chatsPage);
+    app.get('/chat/new',newChat);
+    app.get('/chats:chat_id',oneChat);
+    app.post('/send/message',sendMessage);
 }
