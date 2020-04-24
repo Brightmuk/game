3366abe74026cb889acf82dc4e5368b31a2b2234
Brightmuk <be.rightmuk@gmail.com>
2020-04-24 21:14:32 +0300

    Level one game play

--- public/assets/js/start.js
+++ public/assets/js/start.js
@@ -1,3 +1,8 @@
+setTimeout( () => {
+    document.getElementById('begin').style.display='none';
+    document.getElementById('next').style.display='inline';
+  }, 4600)
+ 
 
 //generate random degree
 function rotation(){
@@ -26,43 +31,89 @@ function rotation(){
     div.style.transform='rotate(-'+deg+'deg)';
     div.style.transform='rotate(6 turn)';
 
+     //render the chosen box
+     function render(result,fate){
+        setTimeout( () => {
+        document.getElementById('result').innerHTML=result;  
+        document.getElementById('spin').style.display="none";
+        document.getElementById('fate').innerHTML=fate;
+
+     
+        }, 5700)
+       }
     //getting value of result
     if(randomNo<360 && randomNo>324){
         console.log('got a 9')
         var result=9
+        var fate="A royal born"
+        render(result,fate)
     }else if(randomNo<324 && randomNo>288){
         console.log('got a 8')
         var result=8
+        var fate="A high class born"
+        render(result,fate)
      }else if(randomNo<288 && randomNo>252){
         console.log('got a 7')
         var result=7
+         var fate="A middle class born"
+        render(result,fate)
      }else if(randomNo<252 && randomNo>216){
         console.log('got a 6')
         var result=6
+         var fate="A low class born"
+        render(result,fate)
     }else if(randomNo<216 && randomNo>180){
         console.log('got a 5')
         var result=5
+        var fate="A street child"
+        render(result,fate)
     }else if(randomNo<180 && randomNo>144){
         console.log('got a 4')
         var result=4
+        var fate="A royal born"
+        render(result,fate)
     }else if(randomNo<144 && randomNo>108){
         console.log('got a 3')
         var result=3
+        var fate="A high class born"
+        render(result,fate)
     }else if(randomNo<108 && randomNo>72){
         console.log('got a 2')
         var result=2
+        var fate="A middle class born"
+        render(result,fate)
     }else if(randomNo<72 && randomNo>36){
         console.log('got a 1')
         var result=1
+        var fate="A low class born"
+        render(result,fate)
     }else if(randomNo<36 && randomNo>0){
         console.log('got a 0')
-        var result=0
-    }else{console.log('SPin again')}
-  
+        var result='o'
+        var fate="A street child"
+        render(result,fate)
+    }else{console.log('SPin again')
+    var result="Your lucky, spin again"
+    document.getElementById('lucky').innerHTML=result;
+    }
+       
   
     setTimeout( () => {
-        document.getElementById('I').innerHTML=result;
-      }, 5500)
+        // var s = document.getElementById('next').style; s.opacity = 1;
+        // (function fade(){(s.opacity-=.1)<0?s.display="none":setTimeout(fade,400)})();
+       
+      }, 5700)
      
    
 }
+
+// var styles = `
+// #boxes span{
+//    display:none
+// }
+
+// `
+//  var styleSheet = document.createElement("style")
+//  styleSheet.type = "text/css"
+//  styleSheet.innerText = styles
+//  document.head.appendChild(styleSheet)
